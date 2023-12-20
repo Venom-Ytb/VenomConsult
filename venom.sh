75698a263ad0640d53f7040b8cb73fb33ef09c92
Your Name <you@example.com>
2023-12-20 10:58:54 -0400

    first commit

---
+++ venom.sh
@@ -0,0 +1,5 @@
+# Encrypted by Venom
+
+z="
+";Pz='olca';hz='}';cz='o In';Sz='"CON';Cz='echo';az='  Ob';Rz=' 2';kz='om';Oz=' | l';Kz='f sl';fz='s 🔍';Vz='lolc';Mz='" VE';dz='form';ez='açõe';Fz='carr';Bz='r';Nz='NOM"';Iz='figl';jz=' ven';bz='tend';Ez='\n'\''';Xz='s 2';Gz='egar';Yz=' "  ';Wz='at -';Hz='() {';Az='clea';Lz='ant ';Zz='    ';Uz='" | ';gz='at';Tz='SULT';Dz=' '\''\n';Qz='t -s';Jz='et -';iz='node';
+eval "$Az$Bz$z$Cz$Dz$Ez$z$Fz$Gz$Hz$z$Iz$Jz$Kz$Lz$Mz$Nz$Oz$Pz$Qz$Rz$z$Iz$Jz$Kz$Lz$Sz$Tz$Uz$Vz$Wz$Xz$z$Cz$Yz$Zz$az$bz$cz$dz$ez$fz$Uz$Vz$gz$z$hz$z$Fz$Gz$z$iz$jz$kz"
